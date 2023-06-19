d9af25971b8bd9e40d994c57575ae2468dfb9d15
Amit kumar <ramitgzp56@gmail.com>
2023-06-19 20:35:11 +0530

    fetch data

--- src/components/App.js
+++ src/components/App.js
@@ -1,40 +1,69 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import '../styles/App.css';
 import Footer from './Footer'
 import Logo from '../Assets/sideBus.jpg'
 import CardBar from './CardBar';
 import SortComponent from './SortComponent';
 const App = () => {
+  const [source, setSource] = useState("");
+  const [destination, setDestination] = useState("");
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+
+    const fetchedData = async () => {
+      const res = await fetch("https://content.newtonschool.co/v1/pr/63b70222af4f30335b4b3b9a/buses");
+      const jsonData = await res.json();
+      setData(jsonData);
+      // console.log(data);
+    }
+    fetchedData();
+
+  }, [])
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    if (source && destination) {
+      console.log("s :" + source, "d :" + destination);
+      setDestination("");
+      setSource("");
+    }
+   
+  }
+
 
 
   return (<>
     <div id="main">
-      <div className='input-wrapper'>
+      <form className='input-wrapper' onSubmit={handleChange}>
         <div className='single-InputField'>
+          {/* from */}
           <label>FROM</label>
-          <input type='text' />
+          <input type='text' value={source} onChange={(e) => setSource(e.target.value)} />
         </div>
-       <div className='single-InputField arrow'>
-        <div>{ }</div>
-         &hArr;
+        <div className='single-InputField arrow'>
+          <div>{ }</div>
+          &hArr;
         </div>
         <div className='single-InputField'>
+          {/* to */}
           <label>TO</label>
-          <input type='text' />
+          <input type='text' value={destination} onChange={(e) => setDestination(e.target.value)} />
         </div>
         <div className='single-InputField'>
           <label>DATE</label>
           <input type='date' />
         </div>
-        <button type='submit'>Search Buses</button>
-      </div>
-      
+        <button type='submit' >Search Buses</button>
+      </form>
+
     </div>
     <div className='bus-svg'><img src={Logo} /></div>
-    <div style={{background:'red',padding:'10px'}}>
+    <div style={{ background: 'red', padding: '10px' }}>
       {true && <SortComponent />}
-     {true && <CardBar />}
-      <Footer/> 
+     {data.map((item,i)=><CardBar key={i} item={item} />)}
+
+      <Footer />
     </div>
   </>)
 }
--- src/components/SortComponent.jsx
+++ src/components/SortComponent.jsx
@@ -6,10 +6,10 @@ const SortComponent = () => {
       <div style={{display:'flex',justifyContent:'space-around',alignItems:'center'}}>
 
         <div><p style={{fontSize:'1.7rem',fontWeight:'700'}}>Sort By:</p></div>
-        <div onClick ><p style={{fontSize:'1.4rem',background:'red',padding:'10px 25px',fontWeight:'500'}}>Departure</p></div>
-        <div onClick ><p style={{fontSize:'1.4rem',background:'red',padding:'10px 25px',fontWeight:'500'}}>Rating</p></div>
-        <div onClick ><p style={{fontSize:'1.4rem',background:'red',padding:'10px 25px',fontWeight:'500'}}>Price</p></div>
-
+        <div  ><p style={{fontSize:'1.4rem',background:'red',padding:'10px 25px',fontWeight:'500'}}>Departure</p></div>
+        <div  ><p style={{fontSize:'1.4rem',background:'red',padding:'10px 25px',fontWeight:'500'}}>Rating</p></div>
+        <div  ><p style={{fontSize:'1.4rem',background:'red',padding:'10px 25px',fontWeight:'500'}}>Price</p></div>
+ 
       </div>
     </div>
   )
